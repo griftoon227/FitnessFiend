6a3b7122bcf8f815b0a54bd14133054e19eead65
Griffin Flaxman <griftoon@gmail.com>
2020-02-29 19:57:13 -0500

    Updated version software

--- app/src/main/java/com/example/grift/fitnessfiend/MainActivity.java
+++ app/src/main/java/com/example/grift/fitnessfiend/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.grift.fitnessfiend;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //initialize firebase auth
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.signInWithEmailAndPassword("griffinflaxman27@gmail.com", "mynewjacket456")
+            .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(getApplicationContext(), "Authenticated", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        //updateUI(firebaseAuth.getCurrentUser());
     }
 }
